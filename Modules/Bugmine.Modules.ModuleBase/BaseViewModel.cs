c5da85639d58e9adb9de0504bd2bf72b38006f27
sTodorov <todorovSvetoslav0404@gmail.com>
2012-11-22 20:58:16 +0100

    Add Module base: baseviewmodel and RelayCommand

---
+++ Modules/Bugmine.Modules.ModuleBase/BaseViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugmine.Modules.ModuleBase
+{
+	public class BaseViewModel : INotifyPropertyChanged
+	{
+		private readonly Dictionary<string, PropertyChangedEventArgs> eventArgsCache;
+
+		protected BaseViewModel()
+		{
+			eventArgsCache = new Dictionary<string, PropertyChangedEventArgs>();
+		}
+
+		#region Implementation of INotifyPropertyChanged
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged(string propertyName)
+		{
+			PropertyChangedEventArgs args;
+			if (!eventArgsCache.TryGetValue(propertyName, out args))
+			{
+				args = new PropertyChangedEventArgs(propertyName);
+				eventArgsCache.Add(propertyName, args);
+			}
+
+			OnPropertyChanged(args);
+		}
+
+		protected void OnPropertyChanged(PropertyChangedEventArgs args)
+		{
+			var handler = PropertyChanged;
+			if (handler != null)
+				handler(this, args);
+		}
+
+		#endregion
+	}
+}
