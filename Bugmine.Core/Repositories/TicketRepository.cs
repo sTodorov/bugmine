989e7c203e17e6807a7ff4b894636deec03ace8e
sTodorov <todorovSvetoslav0404@gmail.com>
2012-11-22 21:00:29 +0100

    Add TicketRepository implementation

---
+++ Bugmine.Core/Repositories/TicketRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Bugmine.Core.Repositories
+{
+	public class TicketRepository : ITicketRepository
+	{
+		public List<Models.Ticket> GetTickets()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
