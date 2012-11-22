9918f91eaee411da384711e34bd0c2ba243b64c5
sTodorov <todorovSvetoslav0404@gmail.com>
2012-11-22 20:58:38 +0100

    Move Loginmodule

--- Bugmine.UI/Shell.xaml.cs
+++ Bugmine.UI/Shell.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Bugmine.Core.Repositories;
 
 namespace Bugmine.UI
 {
@@ -22,7 +23,7 @@ namespace Bugmine.UI
 	{
 		public Shell()
 		{
-			InitializeComponent();
+			InitializeComponent();	
 		}
 	}
 }
---
+++ Modules/Bugmine.Modules.LogIn/ViewModels/LoginViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Bugmine.Modules.ModuleBase;
+using Microsoft.Practices.Prism.Commands;
+
+namespace Bugmine.Modules.LogIn.ViewModels
+{
+	public class LoginViewModel : BaseViewModel
+	{
+		public ICommand LoginCommand { get { return _loginCommand; } }
+		private ICommand _loginCommand;
+
+		private string _loginKey;
+		public string LoginKey
+		{
+			get { return _loginKey; }
+			set
+			{
+				if (value != _loginKey)
+				{
+					_loginKey = value;
+
+					OnPropertyChanged("LoginKey");
+				}
+			}
+		}
+
+		public LoginViewModel()
+		{
+			_loginCommand = new RelayCommand(c => this.LoginUser(), c => this.CanLoginUser);
+		}
+
+		private bool CanLoginUser
+		{
+			get
+			{
+				return !string.IsNullOrEmpty(_loginKey);
+			}
+		}
+
+		private void LoginUser()
+		{
+			//todo : Login via service
+		}
+
+
+	}
+}
