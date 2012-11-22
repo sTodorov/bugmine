2eff7bac915f2923dfbe1682b53d1fa8620d7af5
Stan <s.todorov@diract-it.nl>
2012-11-22 08:12:03 +0100

    Added and configured Prism

--- Bugmine.UI/App.xaml.cs
+++ Bugmine.UI/App.xaml.cs
@@ -13,5 +13,11 @@ namespace Bugmine.UI
 	/// </summary>
 	public partial class App : Application
 	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+			Bootstrapper strap = new Bootstrapper();
+			strap.Run();
+		}
 	}
 }
---
+++ Bugmine.UI/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Prism.UnityExtensions;
+
+namespace Bugmine.UI
+{
+	public class Bootstrapper : UnityBootstrapper
+	{
+		protected override System.Windows.DependencyObject CreateShell()
+		{
+			return new Shell();
+		}		
+
+		protected override void InitializeShell()
+		{
+			base.InitializeShell();
+
+			App.Current.MainWindow = (Window)this.Shell;
+			App.Current.MainWindow.Show();
+		}
+
+		protected override void ConfigureModuleCatalog()
+		{
+			base.ConfigureModuleCatalog();
+		}
+	}
+}
