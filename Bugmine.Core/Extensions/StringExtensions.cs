258994bd9b2dd13eb5e792d000e6f15d6c78329e
sTodorov <todorovSvetoslav0404@gmail.com>
2012-11-25 12:01:43 +0100

    Added string extensions

---
+++ Bugmine.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+	public static class StringExtensions
+	{
+		public static void ThrowIfNullOrEmpty(this string instance, string message = null)
+		{
+			if (string.IsNullOrEmpty(instance))
+				throw new InvalidOperationException(message ?? "String can't be null or empty");
+		}
+	}
+}
