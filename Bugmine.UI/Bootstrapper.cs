7c6c1d76e98b09992ba594e8c2a8ea573037edee
sTodorov <todorovSvetoslav0404@gmail.com>
2012-11-25 12:02:56 +0100

    Added initializion of AppData.
    Create a AddData file in the settings -> Roaming -> Bugmine to store the
    application data.
    
    
    Signed-off-by: sTodorov <todorovSvetoslav0404@gmail.com>

--- Bugmine.UI/Bootstrapper.cs
+++ Bugmine.UI/Bootstrapper.cs
@@ -1,21 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Xml.Linq;
+using Bugmine.Core.Repositories;
+using Bugmine.Core.Repositories.Contracts;
+using Bugmine.Core.Services;
 using Bugmine.Modules.LogIn;
 using Bugmine.Modules.MyPage;
 using Microsoft.Practices.Prism.Modularity;
 using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.Unity;
 
 namespace Bugmine.UI
 {
 	public class Bootstrapper : UnityBootstrapper
 	{
+		private const string _settingsFileName = "settings.xml";
 		protected override System.Windows.DependencyObject CreateShell()
 		{
 			return new Shell();
+		}
+
+		public Bootstrapper()
+			: base()
+		{
+			InitializeAppDataFile();
 		}
 
 		protected override void InitializeShell()
@@ -28,7 +42,10 @@ namespace Bugmine.UI
 
 		protected override void ConfigureContainer()
 		{
-			base.ConfigureContainer();			
+			base.ConfigureContainer();
+
+			Container.RegisterType<IUserService, UserService>();
+			Container.RegisterType<IUserRepository, UserRepository>();
 		}
 
 		protected override void ConfigureModuleCatalog()
@@ -37,6 +54,23 @@ namespace Bugmine.UI
 
 			ModuleCatalog catalog = (ModuleCatalog)this.ModuleCatalog;
 			catalog.AddModule(typeof(LoginModule));
+		}
+
+		//TODO: Abstract this and make the settings strongly typed
+		private void InitializeAppDataFile()
+		{
+			var settingsDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Bugmine");
+			if (!Directory.Exists(settingsDirectory))
+				Directory.CreateDirectory(settingsDirectory);
+
+			var settingFile = Path.Combine(settingsDirectory, _settingsFileName);
+			if (!File.Exists(settingFile))
+			{
+				XDocument doc = new XDocument(new XDeclaration("1.0", "utf-8", "yes"),
+																				new XElement("Settings", new XElement("ApiKey", string.Empty)));
+
+				doc.Save(settingFile);
+			}
 		}
 	}
 }
