352a3dad2e82c28f628d247a416822923abe77c1
sTodorov <todorovSvetoslav0404@gmail.com>
2012-12-04 06:15:49 +0100

    Added ticket service and its interface

---
+++ Bugmine.Core/Services/ITicketService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bugmine.Core.Models;
+
+namespace Bugmine.Core.Services
+{
+	public interface ITicketService
+	{
+		List<Ticket> GetTickets();
+	}
+}
---
+++ Bugmine.Core/Services/TicketService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bugmine.Core.Repositories.Contracts;
+
+namespace Bugmine.Core.Services
+{
+	public class TicketService : ITicketService
+	{
+		private ITicketRepository _ticketRepository;
+
+		public TicketService(ITicketRepository ticketRepo)
+		{
+			_ticketRepository = ticketRepo;
+		}
+
+		public List<Models.Ticket> GetTickets()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
