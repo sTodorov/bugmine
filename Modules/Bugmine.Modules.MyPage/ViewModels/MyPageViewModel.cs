d8fa17731827c4027b8f5eb252c9fbb9eb8c2484
sTodorov <todorovSvetoslav0404@gmail.com>
2012-12-13 06:18:55 +0100

    WIP: Experimenting with ViewModel

--- Modules/Bugmine.Modules.MyPage/ViewModels/MyPageViewModel.cs
+++ Modules/Bugmine.Modules.MyPage/ViewModels/MyPageViewModel.cs
@@ -2,45 +2,63 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Bugmine.Core.Services;
 using Bugmine.Modules.MyPage.Models;
 using ReactiveUI;
+using ReactiveUI.Xaml;
 
 namespace Bugmine.Modules.MyPage.ViewModels
 {
 	public class MyPageViewModel : ReactiveValidatedObject
 	{
-		public List<TicketModel> Tickets { get; set; }
-		private ObservableAsPropertyHelper<List<TicketModel>> _Tickets { get; set; }
+		/// <summary>
+		/// Collection of tickets
+		/// </summary>
+		public ReactiveCollection<TicketModel> Tickets { get; protected set; }
 
+
+
+		/// <summary>
+		/// The service which retrieves the tickets
+		/// </summary>
 		private ITicketService _ticketService;
 
 		public MyPageViewModel() : this(null) { }
 
+
 		public MyPageViewModel(ITicketService ticketService)
 		{
-			Tickets = new List<TicketModel>() { 
-				new TicketModel{
-					Name = "Test ticket 1",
-					Status = "Assigned",
-					TicketNumber = 1234,
-					DueDate = DateTime.Now,
-					EstimatedTime = new TimeSpan(6,30,0),
-					SpentTime = new TimeSpan(2, 0,0),
-					SpentTimeUnCommited = new TimeSpan(4,30,0)
-				},
-				new TicketModel{
-					Name = "Test ticket 2",
-					Status = "Assigned",
-					TicketNumber = 1235,
-					DueDate = DateTime.Now,
-					EstimatedTime = new TimeSpan(6,30,0),
-					SpentTime = new TimeSpan(2, 30,0),
-					SpentTimeUnCommited = new TimeSpan(5,30,0)
+			_ticketService = ticketService;
+
+			LoadTickets = new ReactiveAsyncCommand(null, 0);
+
+			LoadTickets.RegisterAsyncFunction(x => loadTickets().ToObservable().CreateCollection())
+				.ToProperty<MyPageViewModel, ReactiveCollection<TicketModel>>(this, x => x.Tickets);
+
+			Observable.Timer(TimeSpan.FromMinutes(5), RxApp.DeferredScheduler)
+		.InvokeCommand(LoadTickets);
+
+			//LoadTickets.Execute(null);
+		}
+
+		/// <summary>
+		/// A command which exposes the load action for the tickets
+		/// </summary>
+		public ReactiveAsyncCommand LoadTickets { get; private set; }
+
+		private List<TicketModel> loadTickets()
+		{
+			var ticketModel = new List<TicketModel>() { 
+				new TicketModel(){
+						Name = "asd",
+						TicketNumber= 123
 				}
 			};
+
+			return ticketModel;
 		}
 	}
 }
