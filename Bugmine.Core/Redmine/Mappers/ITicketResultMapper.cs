f53f83c066e97cb58dc40100d5f9fcb3a3d5b7fb
sTodorov <todorovSvetoslav0404@gmail.com>
2012-12-16 09:42:20 +0100

    Added ticket mapper to map TicketResult to Ticket Model

---
+++ Bugmine.Core/Redmine/Mappers/ITicketResultMapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bugmine.Core.Models;
+
+namespace Bugmine.Core.Redmine.Mappers
+{
+	public interface ITicketResultMapper
+	{
+		List<Ticket> MapFromTicketResult(TicketsResult result);
+ 	}
+}
---
+++ Bugmine.Core/Redmine/Mappers/TicketResultMapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bugmine.Core.Models;
+
+namespace Bugmine.Core.Redmine.Mappers
+{
+	public class TicketResultMapper : ITicketResultMapper
+	{
+		public TicketResultMapper()
+		{
+			AutoMapper.Mapper.CreateMap<TicketsResult, List<Ticket>>();
+		}
+
+		public List<Ticket> MapFromTicketResult(TicketsResult result)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
