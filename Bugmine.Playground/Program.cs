91bd5356df10c7ffdc4ee292faee05eab3fe47d6
sTodorov <todorovSvetoslav0404@gmail.com>
2012-12-26 12:34:36 +0100

    Added playground

---
+++ Bugmine.Playground/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugmine.Playground
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Observable.Interval(TimeSpan.FromSeconds(5)).Subscribe(_ => Console.WriteLine("Triggered"));
+
+			Console.ReadLine();
+		}
+	}
+}
