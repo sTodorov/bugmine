f09f1aa0ebd3c0bb7cf8a46e3d4a5ebac1f2edcb
sTodorov <todorovSvetoslav0404@gmail.com>
2013-01-01 09:21:43 +0100

    Add navigation method for the login view

--- Bugmine.UI.Controls/Navigation/INavigationController.cs
+++ Bugmine.UI.Controls/Navigation/INavigationController.cs
@@ -8,6 +8,14 @@ namespace Bugmine.UI.Controls.Navigation
 {
 	public interface INavigationController
 	{
+		/// <summary>
+		/// Navigates to the main view
+		/// </summary>
 		void NavigateToMainView();
+
+		/// <summary>
+		/// Navigates to the login view
+		/// </summary>
+		void NavigateToLoginView();
 	}
 }
